afe06f0636a4929e5d604c9533f91758a7bebd22
Suresh-web-dev <dssuresh68@gmail.com>
2025-05-21 10:02:21 -0700

    update settings

--- show_booking/settings.py
+++ show_booking/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for show_booking project.
 
@@ -115,6 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+import os
+from pathlib import Path
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
