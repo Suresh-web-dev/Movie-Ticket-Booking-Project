3fb44ad8f4cea9c73353951c731220d461e08efd
Suresh-web-dev <dssuresh68@gmail.com>
2025-05-21 11:18:17 -0700

    update settings

--- show_booking/settings.py
+++ show_booking/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 import os
 from pathlib import Path
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
