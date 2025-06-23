0f46522e4bb1386effde17cfb623d08aa1dcf395
Suresh-web-dev <dssuresh68@gmail.com>
2025-06-22 19:25:44 -0700

    Setup Cloudinary for media files

--- show_booking/settings.py
+++ show_booking/settings.py
@@ -145,16 +145,12 @@ EMAIL_HOST_PASSWORD = 'ying zlbq jilx qcfy'
 
 
 
-
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-from cloudinary_storage.storage import MediaCloudinaryStorage
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+import os
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
     'API_KEY': os.environ.get('API_KEY'),
-    'API_SECRET': os.environ.get('API_SECRET')
+    'API_SECRET': os.environ.get('API_SECRET'),
 }
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
