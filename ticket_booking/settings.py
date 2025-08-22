137176a7d1def67e714cb0f5c674052a19c2a208
Suresh-web-dev <dssuresh68@gmail.com>
2025-08-21 17:49:04 -0700

    update settings

--- ticket_booking/settings.py
+++ ticket_booking/settings.py
@@ -129,8 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+import os
+
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
